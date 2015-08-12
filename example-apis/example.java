92003bcfcd4067629d57fddcbb157d8fea963616
Buck Shlegeris <bshlegeris@paypal.com>
2015-08-11 20:25:19 -0700

    updated the example

--- example-apis/example.java
+++ example-apis/example.java
@@ -9,21 +9,22 @@ public class Example  {
         int income;
     }
 
-    MagicMultiset<Person> stuff;
+    MagicMultiset<Person> stuff = new MagicMultiset<Person>();
 
     int getAverageIncomeOfOverFifties() {
         int totalIncome = stuff.filter(x -> x.age > 50)
-                    .sum(x -> x.income);
-        int numberOfPeople = stuff.filter(x -> x.age > 50).sum(x -> 1); 
+                .sum(x -> x.income);
+        int numberOfPeople = stuff.filter(x -> x.age > 50).sum(x -> 1);
 
         return totalIncome / numberOfPeople;
     }
-    
+
     int insertPerson(int age, int income) {
         stuff.insert(age, income);
     }
 
-    void removePerson(Person person) {
-        stuff.remove(person);
-    }
+    // if this isn't commented out, my code doesn't know how to optimize getAverageIncomeOfOverFifties.
+    // void removePerson(Person person) {
+    //     stuff.remove(person);
+    // }
 }
